2512ba85e9a7bd67a9cacfa09fa001a8bd71f986
dayikim <wi0910@naver.com>
2025-03-22 19:02:19 +0900

    feat: front 연결을 위한 세팅 test 작업

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, origins=["http://localhost:3000"])
 
-@app.route("/")
-def home():
-    return "Hello, Flask in Docker!"
+@app.route("/api/data")
+def get_data():
+    return jsonify({"message": "React에서 잘 받아옴!"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
